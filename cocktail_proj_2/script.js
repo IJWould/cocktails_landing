f54854736a40b60947be71e3bc0dd13be346c061
Alexander <alexander.al3ksand3r@yandex.ru>
2025-04-23 22:05:42 +0300

    alfa 0.1

--- cocktail_proj_2/script.js
+++ cocktail_proj_2/script.js
@@ -15,57 +15,104 @@
 //     window.addEventListener('resize', () => fitText(element));
 //   });
 
-function centerElementInView(element, container) {
-  const containerWidth = container.offsetWidth;
-  const elementWidth = element.offsetWidth;
-  const elementLeft = element.offsetLeft;
+// function centerElementInView(element, container) {
+//   const containerWidth = container.offsetWidth;
+//   const elementWidth = element.offsetWidth;
+//   const elementLeft = element.offsetLeft;
 
-  const scrollTo = elementLeft - (containerWidth / 2) + (elementWidth / 2);
-  container.scrollTo({
-    left: scrollTo,
-    behavior: 'smooth'
-  });
-}
+//   const scrollTo = elementLeft - (containerWidth / 2) + (elementWidth / 2);
+//   container.scrollTo({
+//     left: scrollTo,
+//     behavior: 'smooth'
+//   });
+// }
 
-window.addEventListener('load', () => {
-  const container = document.querySelector('.cocktails_carusel');
-  const items = document.querySelectorAll('.cocktail_circle_block');
+// window.addEventListener('load', () => {
+//   const container = document.querySelector('.cocktails_carusel');
+//   const items = document.querySelectorAll('.cocktail_circle_block');
 
-  centerElementInView(items[0], container);
+//   centerElementInView(items[0], container);
 
-  // Кликаем по любому элементу — он центрируется
-  items.forEach(item => {
-    item.addEventListener('click', () => {
-      centerElementInView(item, container);
-    });
-  });
-});
+
+//   items.forEach(item => {
+//     item.addEventListener('click', () => {
+//       centerElementInView(item, container);
+//     });
+//   });
+// });
 
 const mediaMobile = window.matchMedia('(max-width: 600px)');
 const windowWidth = document.documentElement.clientWidth
 console.log(windowWidth)
 
-if (windowWidth <= 600){
-    const cocktailRing = document.querySelector('.cocktails_ring')
-    const cocktailsGallery = cocktailRing.parentNode
-    const cocktailRingWidthClildElement = cocktailRing.firstElementChild.offsetWidth
 
+if (windowWidth <= 600) {
+  const cocktailRing = document.querySelector('.cocktails_ring')
+  const cocktailsGallery = cocktailRing.parentNode
+  const cocktailRingWidthClildElement = cocktailRing.firstElementChild.offsetWidth
+  const cocktailRingLenght = document.querySelectorAll('.cocktail_circle_block').length
 
-    cocktailsGallery.style.width = `calc(${cocktailRingWidthClildElement}px)`
+  const marginCocktailBlock = getComputedStyle(cocktailRing.firstElementChild).marginRight
 
+  console.log(marginCocktailBlock)
+  cocktailsGallery.style.width = `calc(${cocktailRingWidthClildElement}px + 10%)`
 
+  const leftButt = document.querySelector('.left_butt');
+  const rightButt = document.querySelector('.right_butt');
+
+  let index = 0;
+  let presentPosition = 0;
+
+  function appearDisappearButt() {
+    if (index === 0) {
+      leftButt.style.display = 'none';
+    }
+    else {
+      leftButt.style.display = 'block';
+    }
+
+    if (index === cocktailRingLenght - 1) {
+      rightButt.style.display = 'none';
+    }
+    else {
+      rightButt.style.display = 'block';
+    }
+  }
+
+  leftButt.onclick = function () {
+    if (index > 0) {
+      index--;
+      presentPosition += cocktailRingWidthClildElement + 32;
+      cocktailRing.style.transition = 'transform 0.3s ease';
+      cocktailRing.style.transform = `translateX(${presentPosition}px)`;
+      appearDisappearButt();
+    }
+  };
+
+  rightButt.onclick = function () {
+    if (index < cocktailRingLenght - 1) {
+      index++;
+      presentPosition -= cocktailRingWidthClildElement + 32;
+      cocktailRing.style.transition = 'transform 0.3s ease';
+      cocktailRing.style.transform = `translateX(calc(${presentPosition}px))`;
+      appearDisappearButt();
+    }
+  };
+
+  appearDisappearButt();
 }
 
-function addDeleteAttributes(elem, attrName){
-  if(elem.hasAttribute(attrName)){
+
+function addDeleteAttributes(elem, attrName) {
+  if (elem.hasAttribute(attrName)) {
     elem.removeAttribute(attrName);
   }
-  else{
+  else {
     elem.setAttribute(attrName, true);
   };
 };
 
-function addReplaceClassShiftAnimation(element, className){
+function addReplaceClassShiftAnimation(element, className) {
   element.classList.toggle(className)
 }
 
@@ -77,35 +124,45 @@ function appearDisappearAboutBlock(cocktailBlock) {
   const FlippedCocktailBlock = cocktailBlock.target.closest('.cocktail_circle_block');
   const CocktailBlocks = document.querySelector('.cocktails_ring');
 
+  // const movieSection = document.querySelector('.movie_cocktails_section')
+
 
   const ActivatedInformationBlock = reciepAndHistoryBlock.querySelector('[appeared-information]');
   const flippedCocktailCard = CocktailBlocks.querySelector('[activated-flipped-cocktail-card]');
 
+
+
   if (ActivatedInformationBlock && ActivatedInformationBlock !== CocktailInformationBlock) {
+
     addDeleteAttributes(ActivatedInformationBlock, 'appeared-information');
     ActivatedInformationBlock.classList.remove('cocktail_reciep_and_history');
-
-    flippedCocktailCard.classList.remove('flipped')
+    // movieSection.classList.remove('movie_animation');
+    flippedCocktailCard.classList.remove('flipped');
     addDeleteAttributes(flippedCocktailCard, 'activated-flipped-cocktail-card');
 
   }
 
-
   if (cocktailBlockParentMainClassName === 'flipper') {
+    document.querySelector('.information').classList.add('scaleup')
     CocktailInformationBlock.classList.toggle('cocktail_reciep_and_history');
     addDeleteAttributes(FlippedCocktailBlock, 'activated-flipped-cocktail-card');
     addDeleteAttributes(CocktailInformationBlock, 'appeared-information');
+
+    const scrollHeight = window.innerWidth <= 600 ? 1 : 0.5;
+    window.scrollBy({
+      top: window.innerHeight * scrollHeight,
+      behavior: 'smooth'
+    });
+    // movieSection.classList.add('movie_animation_appear');
   }
-  if (!flippedCocktailCard){
-    document.querySelector('.movie_cocktails_section').classList.toggle('shiftTop')
-  }
+
 }
 
 document.querySelectorAll('.cocktail_circle_block').forEach(cocktail_card => {
-    cocktail_card.addEventListener('click', function () {
-      this.classList.toggle('flipped');
-    });
+  cocktail_card.addEventListener('click', function () {
+    this.classList.toggle('flipped');
   });
+});
 
 
-document.querySelector('.cocktails_ring').addEventListener('click', (event)=> appearDisappearAboutBlock(event));
+document.querySelector('.cocktails_ring').addEventListener('click', (event) => appearDisappearAboutBlock(event));
